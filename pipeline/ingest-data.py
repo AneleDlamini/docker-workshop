c429e60bb7357b53193a1c3dae477657387cd461
Silindokuhle Anele Dlamini <85555766+AneleDlamini@users.noreply.github.com>
2026-09-25 11:00:13 +0000

    ingesting data to pipeline

--- pipeline/ingest-data.py
+++ pipeline/ingest-data.py
@@ -4,6 +4,7 @@
 
 
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
@@ -39,19 +40,17 @@ parse_dates = [
 # ##### Upload data by chunks - create ann iterator to loop through
 
 
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-
-    year = 2021
-    month = 1
-
-    chunksize = 100000
-    
-    table_name = 'yellow_taxi_data'
+@click.command()
+@click.option('--pg-user', default='root', show_default=True)
+@click.option('--pg-pass', default='root', show_default=True)
+@click.option('--pg-host', default='localhost', show_default=True)
+@click.option('--pg-port', default=5432, type=int, show_default=True)
+@click.option('--pg-db', default='ny_taxi', show_default=True)
+@click.option('--year', default=2021, type=int, show_default=True)
+@click.option('--month', default=1, type=int, show_default=True)
+@click.option('--chunksize', default=100000, type=int, show_default=True)
+@click.option('--table-name', default='yellow_taxi_data', show_default=True)
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, table_name):
     
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
